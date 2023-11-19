ea8f2ae1acb064c66f2073361268f08545e70e90
Bia <fgG>
2023-11-19 11:47:08 -0300

    RecebeSV

--- src/mainRecebe.c
+++ src/mainRecebe.c
@@ -17,27 +17,6 @@ int len = 0;
 
 
 void utc() {
-    struct timeval current_time;
-    struct tm *time_info;
-
-    // Obtém o tempo atual
-    gettimeofday(&current_time, NULL);
-
-    // Converte para UTC
-    time_info = gmtime(&current_time.tv_sec);
-
-    // Formata e imprime a hora em UTC com milissegundos
-    printf("%d-%02d-%02d %02d:%02d:%02d.%03ld\n",
-           1900 + time_info->tm_year, time_info->tm_mon + 1, time_info->tm_mday,
-           time_info->tm_hour, time_info->tm_min, time_info->tm_sec,
-           current_time.tv_usec / 1000);
-}
-
-static int contador = 0; // Declara um contador como estático para preservar seu valor entre chamadas
-static int contadorSV = 0; // Declara um contador como estático para preservar seu valor entre chamadas
-
-void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data) {
-		gettimeofday(&iniciorecebimentno, NULL);
 		struct timespec ts;
 		clock_gettime(CLOCK_REALTIME, &ts);
 
@@ -48,30 +27,37 @@ void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_cha
 		strftime(formattedTime, sizeof(formattedTime), "%H:%M:%S", &tm);
 
 		printf("Hora recebimento: %s.%09ld\n", formattedTime, ts.tv_nsec);
+}
+
+static int contador = 0; // Declara um contador como estático para preservar seu valor entre chamadas
+static int contadorSV = 0; // Declara um contador como estático para preservar seu valor entre chamadas
+
+void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data) {
+
 	if (pkt_data[3] == 0x01) {
 		contador++; // Incrementa o contador dentro do if
+		utc();
 
 		printf("Pacote %d capturado: %d bytes\n",contador, header->len);
-
-
-		
 		gse_sv_packet_filter((unsigned char *) pkt_data, header->len);
-
+		
 		E1Q1SB1.S1.C1.TVTRa_1.Vol.instMag.f = D1Q1SB4.S1.C1.RSYNa_1.gse_inputs_ItlPositions.E1Q1SB1_C1_Positions.C1_TVTR_1_Vol_instMag.f;
+		
 		int length2 = E1Q1SB1.S1.C1.LN0.ItlPositions.send((unsigned char *) pkt_data, 1, 512);  // generate a goose packet, and store the bytes in "buffer"                   // set a value that appears in the dataset used by the "ItlPositions" GOOSE Control
-
 		gse_sv_packet_filter((unsigned char *) pkt_data, length2);
+		
 		float inputValue = D1Q1SB4.S1.C1.RSYNa_1.gse_inputs_ItlPositions.E1Q1SB1_C1_Positions.C1_TVTR_1_Vol_instMag.f;
 		printf("Valor %f\n",inputValue);
+		printf("\n");
 
     }
 	
 	if (pkt_data[3] == 0x04) {
 		contadorSV++; // Incrementa o contador dentro do if
 
+		utc();
+
 		printf("Pacote SV %d capturado: %d bytes\n",contadorSV, header->len);
-
-
 		
 		gse_sv_packet_filter((unsigned char *) pkt_data, header->len);
 
@@ -82,11 +68,9 @@ void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_cha
 		gse_sv_packet_filter((unsigned char *) pkt_data, length2);
 		float inputValue = D1Q1SB4.S1.C1.RSYNa_1.gse_inputs_ItlPositions.E1Q1SB1_C1_Positions.C1_TVTR_1_Vol_instMag.f;
 		printf("Valor %f\n",inputValue);
-
-    }	
 		printf("\n");
 
-
+    }	
 }
 
 pcap_t *initWinpcap() {
@@ -125,7 +109,7 @@ int main() {
 	initialise_iec61850();
 	fp = initWinpcap();
 
-	printf("Inicio captura de pacote: ");
+	printf("Inicio captura de pacote: \n ");
 	pcap_loop(fp, 50, packet_handler,NULL);
 	printf("\n");
 
