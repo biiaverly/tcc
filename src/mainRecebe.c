f4c2406328094e9afdc9ff3938ca9bfed7acfcbb
Bia <fgG>
2023-11-19 13:43:21 -0300

    recebe csv

--- src/mainRecebe.c
+++ src/mainRecebe.c
@@ -9,6 +9,7 @@
 struct timeval inicioProcessamento, fimProcessamento;
 #define BUFFER_LENGTH	2048
 struct timeval iniciorecebimentno, fimrecebimento,inicioEnvioPacotes,fimEnvioPacotes,inicioEnvioPacote,fimEnvioPacote;
+FILE *file;
 
 pcap_t *fp;
 char errbuf[PCAP_ERRBUF_SIZE];
@@ -16,7 +17,8 @@ unsigned char buf[BUFFER_LENGTH] = {0};
 int len = 0;
 
 
-void utc() {
+
+char* utc() {
 		struct timespec ts;
 		clock_gettime(CLOCK_REALTIME, &ts);
 
@@ -29,36 +31,65 @@ void utc() {
 		printf("Hora recebimento: %s.%09ld\n", formattedTime, ts.tv_nsec);
 }
 
+// Função para formatar os valores em uma string
+char* formatString(char* hora, int contador, int len, float valor) {
+    // Aloca espaço para a string resultante
+    char* result = (char*)malloc(256);  // Ajuste o tamanho conforme necessário
+
+    // Obtém o tempo atual
+    struct timeval current_time;
+    gettimeofday(&current_time, NULL);
+    struct tm* time_info = gmtime(&current_time.tv_sec);
+
+    // Formata os valores na string
+    sprintf(result, "%d-%02d-%02d %02d:%02d:%02d.%03ld,%f,%d,%d",
+            1900 + time_info->tm_year, time_info->tm_mon + 1, time_info->tm_mday,
+            time_info->tm_hour, time_info->tm_min, time_info->tm_sec,
+            current_time.tv_usec / 1000, valor, contador, len);
+
+    // Retorna a string resultante
+    return result;
+}
+
 static int contador = 0; // Declara um contador como estático para preservar seu valor entre chamadas
 static int contadorSV = 0; // Declara um contador como estático para preservar seu valor entre chamadas
 
 void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data) {
 
-	if (pkt_data[3] == 0x01) {
-		contador++; // Incrementa o contador dentro do if
+
+		if (file == NULL) {
+			fprintf(stderr, "Erro ao abrir o arquivo para escrita.\n");
+			return ;
+		}
+
+
+
+
 		utc();
 
+	if (pkt_data[3] == 0x01) {
+		contador++; // Incrementa o contador dentro do if
+
 		printf("Pacote %d capturado: %d bytes\n",contador, header->len);
+
 		gse_sv_packet_filter((unsigned char *) pkt_data, header->len);
-		
+
 		E1Q1SB1.S1.C1.TVTRa_1.Vol.instMag.f = D1Q1SB4.S1.C1.RSYNa_1.gse_inputs_ItlPositions.E1Q1SB1_C1_Positions.C1_TVTR_1_Vol_instMag.f;
-		
 		int length2 = E1Q1SB1.S1.C1.LN0.ItlPositions.send((unsigned char *) pkt_data, 1, 512);  // generate a goose packet, and store the bytes in "buffer"                   // set a value that appears in the dataset used by the "ItlPositions" GOOSE Control
 		gse_sv_packet_filter((unsigned char *) pkt_data, length2);
 		
+		
 		float inputValue = D1Q1SB4.S1.C1.RSYNa_1.gse_inputs_ItlPositions.E1Q1SB1_C1_Positions.C1_TVTR_1_Vol_instMag.f;
 		printf("Valor %f\n",inputValue);
-		printf("\n");
+ 
+		fprintf(file, "Carlos,28,Belo Horizonte\n");
 
     }
 	
 	if (pkt_data[3] == 0x04) {
 		contadorSV++; // Incrementa o contador dentro do if
 
-		utc();
-
 		printf("Pacote SV %d capturado: %d bytes\n",contadorSV, header->len);
-		
 		gse_sv_packet_filter((unsigned char *) pkt_data, header->len);
 
 		len = E1Q1SB1.S1.C1.LN0.rmxuCB.update(buf);
@@ -66,11 +97,16 @@ void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_cha
 		int length2 = E1Q1SB1.S1.C1.LN0.ItlPositions.send((unsigned char *) pkt_data, 1, 512);  // generate a goose packet, and store the bytes in "buffer"                   // set a value that appears in the dataset used by the "ItlPositions" GOOSE Control
 		printf("Valor : %f\n", D1Q1SB4.S1.C1.exampleMMXU_1.sv_inputs_rmxuCB.E1Q1SB1_C1_rmxu[15].C1_RMXU_1_AmpLocPhsA.instMag.f);
 		gse_sv_packet_filter((unsigned char *) pkt_data, length2);
-		float inputValue = D1Q1SB4.S1.C1.RSYNa_1.gse_inputs_ItlPositions.E1Q1SB1_C1_Positions.C1_TVTR_1_Vol_instMag.f;
-		printf("Valor %f\n",inputValue);
-		printf("\n");
+		float inputValue = D1Q1SB4.S1.C1.exampleMMXU_1.sv_inputs_rmxuCB.E1Q1SB1_C1_rmxu[15].C1_RMXU_1_AmpLocPhsA.instMag.f;
 
+    	char* hora = utc();
+    	char* stringFormatada = formatString(hora, contadorSV, header->len, inputValue);
+		// Escreve cabeçalho (opcional)
+		fprintf(file, "%s\n", stringFormatada);
     }	
+		printf("\n");
+		// Fecha o arquivo
+
 }
 
 pcap_t *initWinpcap() {
@@ -103,18 +139,20 @@ pcap_t *initWinpcap() {
 	return fpl;
 }
 
+
 int main() {
 	int len = 0;
-
 	initialise_iec61850();
 	fp = initWinpcap();
 
-	printf("Inicio captura de pacote: \n ");
-	pcap_loop(fp, 50, packet_handler,NULL);
+	file = fopen("recebe.csv", "a"); // Abre o arquivo para escrita (modo de adição)
+
+	printf("Inicio captura de pacote: ");
+	pcap_loop(fp, 100, packet_handler,NULL);
 	printf("\n");
 
-	
 	pcap_close(fp);
+	fclose(file);
 
 
 	return 0;
