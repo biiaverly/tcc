8410d9749a2339d1e3ba0894058ad57613d0200f
Bia <fgG>
2023-11-19 20:00:57 -0300

    envia SV final

--- src/mainSV.c
+++ src/mainSV.c
@@ -13,11 +13,42 @@ pcap_t *fp;
 char errbuf[PCAP_ERRBUF_SIZE];
 unsigned char buf[BUFFER_LENGTH] = {0};
 int len = 0;
+FILE *file;
+int contador = 0;
 
 void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data) {
 	gse_sv_packet_filter((unsigned char *) pkt_data, header->len);
 }
+char* utc() {
+		struct timespec ts;
+		clock_gettime(CLOCK_REALTIME, &ts);
 
+		struct tm tm;
+		localtime_r(&ts.tv_sec, &tm);
+
+		char formattedTime[20];
+		strftime(formattedTime, sizeof(formattedTime), "%H:%M:%S", &tm);
+
+		printf("Hora envio: %s.%09ld\n", formattedTime, ts.tv_nsec);
+}
+char* formatString(char* hora, int contador, int len, float valor) {
+    // Aloca espaço para a string resultante
+    char* result = (char*)malloc(256);  // Ajuste o tamanho conforme necessário
+
+    // Obtém o tempo atual
+    struct timeval current_time;
+    gettimeofday(&current_time, NULL);
+    struct tm* time_info = gmtime(&current_time.tv_sec);
+
+    // Formata os valores na string
+    sprintf(result, "%d-%02d-%02d %02d:%02d:%02d.%06ld,%f,%d,%d",
+            1900 + time_info->tm_year, time_info->tm_mon + 1, time_info->tm_mday,
+            time_info->tm_hour, time_info->tm_min, time_info->tm_sec,
+            current_time.tv_usec, valor, contador, len);
+
+    // Retorna a string resultante
+    return result;
+}
 
 void enviarPacoteSV(float valueSV, pcap_t *fp) {
     unsigned char buf[BUFFER_LENGTH] = {0};
@@ -31,6 +62,8 @@ void enviarPacoteSV(float valueSV, pcap_t *fp) {
 	for (i = 0; i < E1Q1SB1.S1.C1.LN0.rmxuCB.noASDU; i++) {
 		len = E1Q1SB1.S1.C1.LN0.rmxuCB.update(buf);
 		if (len > 0) {
+			contador++;
+			char* hora = utc();
 			printf("len value SV: %d\n", len);
 			pcap_sendpacket(fp, buf, len);
 			gse_sv_packet_filter(buf, len);
@@ -40,6 +73,10 @@ void enviarPacoteSV(float valueSV, pcap_t *fp) {
 			fflush(stdout);
 			printf("Valor de i: %d \n",i);
 			printf("\n");
+			int inputValue = D1Q1SB4.S1.C1.exampleMMXU_1.sv_inputs_rmxuCB.E1Q1SB1_C1_rmxu[15].C1_RMXU_1_AmpLocPhsA.instMag.f;
+    		char* stringFormatada = formatString(hora, contador,len, inputValue);
+		// Escreve cabeçalho (opcional)
+			fprintf(file, "%s\n", stringFormatada);
 		}
 
 	}
@@ -74,18 +111,7 @@ pcap_t *initWinpcap() {
 	return fpl;
 }
 
-void utc() {
-		struct timespec ts;
-		clock_gettime(CLOCK_REALTIME, &ts);
 
-		struct tm tm;
-		localtime_r(&ts.tv_sec, &tm);
-
-		char formattedTime[20];
-		strftime(formattedTime, sizeof(formattedTime), "%H:%M:%S", &tm);
-
-		printf("Hora recebimento: %s.%09ld\n", formattedTime, ts.tv_nsec);
-}
 
 int main() {
 
@@ -94,11 +120,11 @@ int main() {
     initialise_iec61850();
     fp = initWinpcap();
 
+	file = fopen("envia.csv", "a"); // Abre o arquivo para escrita (modo de adição)
 
-	int max_delay = 1000;
+	int max_delay = 300;
 	int delay = 0;	
     while (delay <= max_delay) {
-		utc();
     	enviarPacoteSV(valueSV, fp);
 		usleep(208); // Espera por 208 nanossegundos
 	    delay++; // Incrementa delay (ou use a lógica desejada para ajustar o valor de delay)
@@ -107,6 +133,7 @@ int main() {
 
 	fflush(stdout);
 	pcap_close(fp);
+	fclose(file);
 
 
 	return 0;
