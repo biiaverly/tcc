ebea7242bd7ef7f8907ed040a1e0cba4c744fa10
Bia <fgG>
2023-11-30 23:30:25 -0300

    mainSV comentado.

--- src/mainSV.c
+++ src/mainSV.c
@@ -8,7 +8,9 @@
 #include "iec61850.h"
 #include "json/json.h"
 #define BUFFER_LENGTH	2048
+
 #define PORT 12345
+#define SERVER_ADDRESS "192.168.0.17"  // Substitua pelo endereço do servidor
 
 pcap_t *fp;
 char errbuf[PCAP_ERRBUF_SIZE];
@@ -16,31 +18,50 @@ unsigned char buf[BUFFER_LENGTH] = {0};
 int len = 0;
 FILE *file;
 int contador = 0;
+int sockfd; // Descritor do socket
 
 
-void syncTime(int sockfd) {
-    struct timeval client_time;
 
-    // Obter tempo local com milissegundos
-    gettimeofday(&client_time, NULL);
 
-    // Enviar tempo do cliente ao servidor
-    send(sockfd, &client_time, sizeof(client_time), 0);
+///// Metodo criado para pegar a hora do notebook e testar o escorregamento dos relogios.
+char* synchronizeClock() {
+    struct timeval tv;
+ 
+    // Solicita o tempo ao servidor
+        char request[] = "GET_TIME";
+        if (write(sockfd, request, sizeof(request)) == -1) {
+            perror("Erro ao solicitar o tempo ao servidor");
+            exit(EXIT_FAILURE);
+        }
 
-    // Receber tempo do servidor
-    recv(sockfd, &client_time, sizeof(client_time), 0);
+    // Recebe o tempo do servidor
+        if (read(sockfd, &tv, sizeof(struct timeval)) == -1) {
+            perror("Erro ao receber o tempo do servidor");
+            exit(EXIT_FAILURE);
+        }
 
-    // Converter a estrutura timeval para uma estrutura tm
-    struct tm* server_tm = localtime(&client_time.tv_sec);
+    // Aloca espaço para a string resultante
+        char* formattedTime = (char*)malloc(32);  // Ajuste o tamanho conforme necessário
 
-    // Imprimir a hora formatada com milissegundos
-    printf("Tempo recebido do servidor: %02d:%02d:%02d.%05ld\n",
-           server_tm->tm_hour, server_tm->tm_min, server_tm->tm_sec, client_time.tv_usec / 100);
+    // Metodo com CSV.
+        sprintf(formattedTime, "%02d:%02d:%02d.%06ld",
+                localtime(&tv.tv_sec)->tm_hour,
+                localtime(&tv.tv_sec)->tm_min,
+                localtime(&tv.tv_sec)->tm_sec,
+                tv.tv_usec);
+
+    // Metodo sem CSV.
+        printf("Tempo recebido: %02d:%02d:%02d.%06ld\n", 
+               localtime(&tv.tv_sec)->tm_hour,
+               localtime(&tv.tv_sec)->tm_min,
+               localtime(&tv.tv_sec)->tm_sec,
+               tv.tv_usec);   
+
+    return formattedTime;
 }
 
-
-// Metodo criada para pegar hora atual.
-//// FATLOU O SINCRONISMO DE TEMPO COM UMA FONTE CONFIAVEL
+//// Metodo criada para pegar hora atual.
+//// FATLOU O SINCRONISMO DE TEMPO COM UMA FONTE CONFIAVEL CRIADO O ( synchronizeClock)
 char* utc() {
 		struct timespec ts;
 		clock_gettime(CLOCK_REALTIME, &ts);
@@ -55,20 +76,27 @@ char* utc() {
 }
 
 //// Metodo criado para formatar os dados para salvar no CSV.
-char* formatString(int contador, int len, float valor) {
+char* formatString(char* hora,int contador, int len, float valor) {
     // Aloca espaço para a string resultante
     char* result = (char*)malloc(256);  // Ajuste o tamanho conforme necessário
 
-    // Obtém o tempo atual
-    struct timeval current_time;
-    gettimeofday(&current_time, NULL);
-    struct tm* time_info = gmtime(&current_time.tv_sec);
+ // // ------------------ Metodo com CSV. 
+//   // Pega hora atual.
+//     struct timeval current_time;
+//     gettimeofday(&current_time, NULL);
+//     struct tm* time_info = gmtime(&current_time.tv_sec)
 
-    // Formata os valores na string
-    sprintf(result, "%d-%02d-%02d %02d:%02d:%02d.%06ld,%f,%d,%d",
-            1900 + time_info->tm_year, time_info->tm_mon + 1, time_info->tm_mday,
-            time_info->tm_hour, time_info->tm_min, time_info->tm_sec,
-            current_time.tv_usec, valor, contador, len);
+    // // Formata os valores na string
+    // sprintf(result, "%d-%02d-%02d %02d:%02d:%02d.%06ld,%f,%d,%d",
+    //         1900 + time_info->tm_year, time_info->tm_mon + 1, time_info->tm_mday,
+    //         time_info->tm_hour, time_info->tm_min, time_info->tm_sec,
+    //         current_time.tv_usec, valor, contador, len);
+
+// //--------------------Metodo utilizando quando a hora vem do synchronizeClock.
+  // Formata os valores na string, incluindo a hora capturada
+    sprintf(result, "%s,%f,%d,%d",
+            hora, valor, contador, len);
+
     return result;
 }
 
@@ -86,23 +114,25 @@ void enviarPacoteSV(float valueSV, pcap_t *fp) {
 		len = E1Q1SB1.S1.C1.LN0.rmxuCB.update(buf);
 		if (len > 0) {
 			contador++;
-			/// enviando pacote SV
-			utc();
+            // //(Analise sem CSV) Printar hora atual.
+			// utc();
 			pcap_sendpacket(fp, buf, len);
-			//// utilizado para analise sem CSV.
-			//// Metodo para decodificar pacotes SV e Goose ( valida se o modelo foi atualizado corretamente ).
+            // printf("Pacote %d enviado: ",contador);
+        // // (Sincronismo relogio) Pegar hora do relogio do notebook. 
+           char* hora = synchronizeClock();
+
+		// // (Analise sem CSV)
+			    //// Metodo para decodificar pacotes SV e Goose ( valida se o modelo foi atualizado corretamente ).
 			// gse_sv_packet_filter(buf, len);
 			// printf("SV A test: %s\n", D1Q1SB4.S1.C1.exampleMMXU_1.sv_inputs_rmxuCB.E1Q1SB1_C1_rmxu[15].C1_RMXU_1_AmpLocPhsA.instMag.f == valueSV ? "passed" : "failed");
 			// printf("SV B test: %s\n", D1Q1SB4.S1.C1.exampleMMXU_1.sv_inputs_rmxuCB.E1Q1SB1_C1_rmxu[15].C1_RMXU_1_AmpLocPhsB.instMag.f == valueSV*2 ? "passed" : "failed");
 			
-	//// Salvando dados no CSV.		
-			// int inputValue = D1Q1SB4.S1.C1.exampleMMXU_1.sv_inputs_rmxuCB.E1Q1SB1_C1_rmxu[15].C1_RMXU_1_AmpLocPhsA.instMag.f;
-			// char* stringFormatada = formatString(contador,len, inputValue);
-			// fprintf(file, "%s\n", stringFormatada);
+	    // // Salvando dados no CSV.		
+			int inputValue = D1Q1SB4.S1.C1.exampleMMXU_1.sv_inputs_rmxuCB.E1Q1SB1_C1_rmxu[15].C1_RMXU_1_AmpLocPhsA.instMag.f;
+			char* stringFormatada = formatString(hora,contador,len, inputValue);
+			fprintf(file, "%s\n", stringFormatada);
 		}
-
 	}
-
 }
 
 /// Inicializando PCAP.
@@ -136,61 +166,56 @@ pcap_t *initWinpcap() {
 
 int main() {
 
-	int numeroPacotes = 300;
+	int numeroPacotes = 299;
 	int pacoteAtual = 0;
 	float valueSV = (float) rand() / (float) RAND_MAX;
 
-    int sockfd;
-    struct sockaddr_in server_addr;
+// // -------------------- Metodo de sincronismo de relogio.
+    struct sockaddr_in serv_addr;
+    // Criação do socket
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+            perror("Erro ao criar o socket");
+            exit(EXIT_FAILURE);
+        }
 
-    // Cria um socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        perror("Erro ao criar o socket");
-        exit(EXIT_FAILURE);
-    }
+    // Configuração do endereço do servidor
+        memset(&serv_addr, 0, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(PORT);
+        if (inet_pton(AF_INET, SERVER_ADDRESS, &serv_addr.sin_addr) <= 0) {
+            perror("Erro ao converter o endereço");
+            exit(EXIT_FAILURE);
+        }
 
-    // Configura o endereço do servidor
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = inet_addr("192.168.0.2");
+    // Conversão do endereço IP do servidor para o formato binário
+        if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+            perror("Erro ao conectar");
+            exit(EXIT_FAILURE);
+        }
+	
+// // Inicializando bibliotecas base.
+    initialise_iec61850();
+    fp = initWinpcap();
+// // -------------------- Metodo com CSV.
+	file = fopen("enviaSV.csv", "a"); // Abre o arquivo para escrita (modo de adição)
+// // Envio dos pacotes.	
+    clock_t inicio = clock();
+	//// loop para envio dos pacotes	
+    while (pacoteAtual <= numeroPacotes) {
+    	enviarPacoteSV(valueSV, fp);
+		usleep(208); // Espera por 208 nanossegundos
+	    pacoteAtual++; 
+	}
+    clock_t fim = clock();
+    double tempoDecorrido = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+    printf("O programa levou %.6f segundos para executar.\n", tempoDecorrido);
 
-    // Conecta-se ao servidor
-    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Erro ao conectar ao servidor");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    // Mantém a sincronização contínua de tempo
-    while (1) {
-        syncTime(sockfd);
-        sleep(1);  // Aguarda 1 segundo antes de enviar a próxima solicitação
-    }
-
-    // Fecha o socket
+	fflush(stdout);
+	pcap_close(fp);
+// // ------- Metodo com CSV.
+	fclose(file);
+// // ------- Metodo com sincronismo de tempo..
     close(sockfd);
-
-	// //// Inicializando bibliotecas base.
-    // initialise_iec61850();
-    // fp = initWinpcap();
-
-	// file = fopen("enviaSV.csv", "a"); // Abre o arquivo para escrita (modo de adição)
-	// clock_t inicio = clock();
-	// //// loop para envio dos pacotes	
-    // while (pacoteAtual <= numeroPacotes) {
-    // 	enviarPacoteSV(valueSV, fp);
-	// 	usleep(208); // Espera por 208 nanossegundos
-	//     pacoteAtual++; 
-	// }
-    // clock_t fim = clock();
-    // double tempoDecorrido = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
-    // printf("O programa levou %.6f segundos para executar.\n", tempoDecorrido);
-
-	// fflush(stdout);
-	// pcap_close(fp);
-	// // fclose(file);
 
 	return 0;
 }
